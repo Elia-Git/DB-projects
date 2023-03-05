48bf41c548c08fb55c7cd1278015b148c37c6a5d
Elia-Git <holeselv17@gmail.com>
2023-03-05 10:47:35 +0100

    modified & stored procedures created

--- Student_DBM/create_tables.sql
+++ Student_DBM/create_tables.sql
@@ -1,62 +1,3 @@
--- Create the Students table
-CREATE TABLE Students (
-  StudentID INT PRIMARY KEY,
-  FirstName VARCHAR(50),
-  LastName VARCHAR(50),
-  Address VARCHAR(100),
-  ContactInfo VARCHAR(50),
-  AdmissionYear INT
-);
-
--- Create the Courses table
-CREATE TABLE Courses (
-  CourseCode VARCHAR(10) PRIMARY KEY,
-  CourseName VARCHAR(50),
-  Instructor VARCHAR(50),
-  CreditHours INT
-);
-
--- Create the Enrollments table
-CREATE TABLE Enrollments (
-  EnrollmentID INT PRIMARY KEY,
-  StudentID INT,
-  CourseCode VARCHAR(10),
-  EnrollmentDate DATE,
-  FOREIGN KEY (StudentID) REFERENCES Students(StudentID),
-  FOREIGN KEY (CourseCode) REFERENCES Courses(CourseCode)
-);
-
--- Create the Attendance table
-CREATE TABLE Attendance (
-  AttendanceID INT PRIMARY KEY,
-  StudentID INT,
-  CourseCode VARCHAR(10),
-  AttendanceDate DATE,
-  AttendanceStatus VARCHAR(10),
-  FOREIGN KEY (StudentID) REFERENCES Students(StudentID),
-  FOREIGN KEY (CourseCode) REFERENCES Courses(CourseCode)
-);
-
--- Create the Fees table
-CREATE TABLE Fees (
-  FeeID INT PRIMARY KEY,
-  StudentID INT,
-  FeeType VARCHAR(50),
-  Amount FLOAT,
-  PaymentDate DATE,
-  FOREIGN KEY (StudentID) REFERENCES Students(StudentID)
-);
-
--- Create the Scholarships table
-CREATE TABLE Scholarships (
-  ScholarshipID INT PRIMARY KEY,
-  StudentID INT,
-  ScholarshipType VARCHAR(50),
-  Amount FLOAT,
-  AwardDate DATE,
-  FOREIGN KEY (StudentID) REFERENCES Students(StudentID)
-);
-
 -- Create the Users table
 CREATE TABLE Users (
   UserID INT PRIMARY KEY,
@@ -72,6 +13,15 @@ CREATE TABLE UserRoles (
   RoleName VARCHAR(50)
 );
 
+-- Create the Students table
+CREATE TABLE Students (
+  StudentID INT PRIMARY KEY,
+  FirstName VARCHAR(50),
+  LastName VARCHAR(50),
+  Address VARCHAR(100),
+  ContactInfo VARCHAR(50),
+  AdmissionYear INT
+);
 -- Create the Permissions table
 CREATE TABLE Permissions (
   PermissionID INT PRIMARY KEY,
@@ -83,4 +33,38 @@ CREATE TABLE Permissions (
   FOREIGN KEY (RoleID) REFERENCES UserRoles(RoleID)
 );
 
-SELECT*from Permissions;
+-- Create a stored procedure for student registration
+CREATE PROCEDURE RegisterStudent (
+  @FirstName VARCHAR(50),
+  @LastName VARCHAR(50),
+  @Address VARCHAR(100),
+  @ContactInfo VARCHAR(50),
+  @AdmissionYear INT,
+  @Username VARCHAR(50),
+  @Password VARCHAR(50)
+)
+AS
+BEGIN
+  -- Insert the student information into the Students table
+  INSERT INTO Students (FirstName, LastName, Address, ContactInfo, AdmissionYear)
+  VALUES (@FirstName, @LastName, @Address, @ContactInfo, @AdmissionYear);
+
+  -- Get the student ID of the newly inserted student
+  DECLARE @StudentID INT;
+  SET @StudentID = SCOPE_IDENTITY();
+
+  -- Insert the user information into the Users table
+  INSERT INTO Users (Username, Password, RoleID)
+  VALUES (@Username, @Password, 2); -- Assuming student role has RoleID 2
+
+  -- Get the user ID of the newly inserted user
+  DECLARE @UserID INT;
+  SET @UserID = SCOPE_IDENTITY();
+
+  -- Insert the student permissions into the Permissions table
+  INSERT INTO Permissions (RoleID, TableName, CanView, CanEdit, CanDelete)
+  VALUES (2, 'Students', 1, 1, 0); -- Assuming student role has RoleID 2
+
+  -- Return the student ID and user ID to the calling application
+  SELECT @StudentID AS StudentID, @UserID AS UserID;
+END;
